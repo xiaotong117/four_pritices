b1c884fa50ce12ee2a2f5b78811f06842e3611ed
Administrator <443091047@qq.com>
2019-05-12 23:54:30 +0800

    000

--- xuping/compare.py
+++ xuping/compare.py
@@ -4,6 +4,9 @@
 import openpyxl
 from xuping import config
 from openpyxl.styles import PatternFill
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
 
 def compare(file):
     wb = openpyxl.load_workbook(file)
@@ -32,4 +35,19 @@ def compare(file):
 
     wb.save(file)
 
+    # 发邮件
+    my_sender = 'zhangt@shinemo.com'  # 发件人邮箱账号，为了后面易于维护，所以写成了变量
+    my_user = 'zhangt@shinemo.com'  # 收件人邮箱账号，为了后面易于维护，所以写成了变量
+    try:
+        msg = MIMEText('填写邮件内容', 'plain', 'utf-8')
+        msg['From'] = formataddr(["发件人邮箱昵称", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["收件人邮箱昵称", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = "主题"  # 邮件的主题，也可以说是标题
+        server = smtplib.SMTP("smtp.exmail.qq.com", 465)  # 发件人邮箱中的SMTP服务器，端口是25
+        server.login(my_sender, "发件人邮箱密码")  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 这句是关闭连接的意思
+
+    except:
+        print('123')# 如果try中的语句没有执行，则会执行下面的ret=False
 
